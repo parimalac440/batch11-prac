9912c7ecbde299bcbb7738be7c749ec9c7761ac5
parimala <cparimala440@gmail.com>
2022-09-30 19:54:59 +0530

    intial commit

---
+++ addition.java
@@ -0,0 +1,15 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		Scanner s=new Scanner(System.in);
+	        System.out.println("Enter num1 value");
+		num1=s.nextInt();
+		Syetem.println("Enter num2 value");
+		num2=s.nextInt();
+		res=num1+num2;
+		System.out.println("The result is");
+	}
+}
+
